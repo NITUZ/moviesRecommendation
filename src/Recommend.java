f286d06d5172f7cfb8dccfcba2e6aa1c3e4a5bbe
karinMishor <karinmi@post.bgu.ac.il>
2018-05-01 17:28:35 +0300

    y

--- src/Recommend.java
+++ src/Recommend.java
@@ -13,9 +13,38 @@ public class Recommend {
         currentUser = _currentUser;
         simUser2User = new HashMap<Integer, Double>();
         MovieScore = new HashMap<Integer, Double>();
-        HashMap<Integer, Calcs> calcs = new HashMap<User, Calcs>();
+        HashMap<Integer, Calcs> calcs = new HashMap<Integer, Calcs>();
     }
+    //sort hashmap by value
+    public LinkedHashMap<Integer, Double> sortHashMapByValues(
+            HashMap<Integer, Double> passedMap) {
+        List<Integer> mapKeys = new ArrayList<>(passedMap.keySet());
+        List<Double> mapValues = new ArrayList<>(passedMap.values());
+        Collections.sort(mapValues);
+        Collections.sort(mapKeys);
 
+        LinkedHashMap<Integer, Double> sortedMap =
+                new LinkedHashMap<>();
+
+        Iterator<Double> valueIt = mapValues.iterator();
+        while (valueIt.hasNext()) {
+            Double val = valueIt.next();
+            Iterator<Integer> keyIt = mapKeys.iterator();
+
+            while (keyIt.hasNext()) {
+                Integer key = keyIt.next();
+                Double comp1 = passedMap.get(key);
+                Double comp2 = val;
+
+                if (comp1.equals(comp2)) {
+                    keyIt.remove();
+                    sortedMap.put(key, val);
+                    break;
+                }
+            }
+        }
+        return sortedMap;
+    }
     public void calcSimUser2Users() {
         double currAVG = currentUser.avgRank;
         Iterator it = currentUser.MoviesRanks.entrySet().iterator();
@@ -44,9 +73,9 @@ public class Recommend {
             Map.Entry pair = (Map.Entry) it.next(); // pair = key-movieID val-rankBycurr
             {
                 double Wij = ((Calcs) pair.getValue()).mone / (Math.sqrt(((Calcs) pair.getValue()).mehane1 * ((Calcs) pair.getValue()).mehane2));
-                simUser2User.put(((User) pair.getKey()), Wij);
-
+                simUser2User.put(((Integer) pair.getKey()), Wij);
             }
+            simUser2User=sortHashMapByValues(simUser2User);
         }
     }
 }
