07ed748b23fc095a86a55a71a7773bcf3afdca61
nogahm <nogahm@post.bgu.ac.il>
2018-05-01 11:01:30 +0300

    a

--- src/GUIController.java
+++ src/GUIController.java
@@ -1,2 +1,17 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
 public class GUIController {
+
+    public ChoiceBox rate;
+
+    ObservableList<String> rateValues= FXCollections.observableArrayList( "1","2","3","4","5");
+
+    @FXML
+    public void initialize() {
+        rate.setItems(rateValues);
+        rate.setValue("choose rate");
+    }
 }
