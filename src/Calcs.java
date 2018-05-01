23ad222bab27c2f3d3c2a6ff183dda853233f442
karinmi <karinmi@post.bgu.ac.il>
2018-05-01 15:30:06 +0300

    creat user % movie

---
+++ src/Calcs.java
@@ -0,0 +1,11 @@
+public class Calcs {
+    public double mone;
+    public double mehane1;
+    public double mehane2;
+
+    public Calcs() {
+        mone=0;
+        mehane1=0;
+        mehane2=0;
+    }
+}
