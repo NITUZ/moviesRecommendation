3bc77c9806224a56c0a216be5d4d2624cc36c73a
karinmi <karinmi@post.bgu.ac.il>
2018-05-01 11:43:12 +0300

    creat user % movie

--- src/Movie.java
+++ src/Movie.java
@@ -1,2 +1,8 @@
+import java.util.List;
+
 public class Movie {
+
+    public int movieID;
+    public String movieName;
+    public List<User> users;
 }
---
+++ src/User.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+
+public class User {
+
+    public int userID;
+    public double avgRank;
+    public HashMap<Integer, Double> MoviesRanks; //key-MovieID , val-movieRank
+}
