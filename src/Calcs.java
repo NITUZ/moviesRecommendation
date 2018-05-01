287502b3d8a7fb5f5fd82e2850fa1c1990f3056d
karinmi <karinmi@post.bgu.ac.il>
2018-05-01 16:55:23 +0300

    recommend

--- src/Calcs.java
+++ src/Calcs.java
@@ -3,9 +3,16 @@ public class Calcs {
     public double mehane1;
     public double mehane2;
 
+    public Calcs(double mone, double mehane1, double mehane2) {
+        this.mone = mone;
+        this.mehane1 = mehane1;
+        this.mehane2 = mehane2;
+    }
+
     public Calcs() {
         mone=0;
         mehane1=0;
         mehane2=0;
+
     }
 }
--- src/Recommend.java
+++ src/Recommend.java
@@ -4,37 +4,47 @@ import java.util.Map;
 
 public class Recommend {
 
-    HashMap<Integer, Double> simUser2User; //key= userID , val= similarity
-    HashMap<Integer, Double> MovieScore; //key-movieID, val-score
-    HashMap<User, Calcs> calcs;
+    HashMap<User,Double> simUser2User; //key= userID , val= similarity
+    HashMap<Integer,Double> MovieScore; //key-movieID, val-score
+    HashMap<User,Calcs> calcs;
+
 
     User currentUser;
 
-    public Recommend(User _currentUser) {
-        currentUser = _currentUser;
-        simUser2User = new HashMap<Integer, Double>();
-        MovieScore = new HashMap<Integer, Double>();
-        HashMap<User, Calcs> calcs = new HashMap<User, Calcs>();
+    public Recommend(User _currentUser ) {
+        currentUser=_currentUser;
+        simUser2User = new   HashMap<User,Double> ();
+        MovieScore = new  HashMap<Integer,Double>();
+        HashMap<User,Calcs> calcs= new  HashMap<User,Calcs>();
+ }
 
-    }
-
-    public double calcSimUser2User() {
+    public void calcSimUser2Users() {
         double currAVG = currentUser.avgRank;
         Iterator it = currentUser.MoviesRanks.entrySet().iterator();
-        double mone1;
+        while (it.hasNext()) { //all movies curr rank
+            Map.Entry pair = (Map.Entry) it.next(); // pair = key-movieID val-rankBycurr
+            {
+                Movie currMovie = ReadFromDB.movies.get(pair.getKey());
+                for (User u : currMovie.users) {//for all users of a curr movie
+                    double raForcurr = currentUser.MoviesRanks.get(pair.getKey());
+                    double mone1 = raForcurr - currAVG;
+                    double ruForCurr = u.MoviesRanks.get(pair.getKey());
+                    double mone2 = ruForCurr - u.avgRank;
+                    if (calcs.containsKey(u)) {
+                        calcs.get(u).mone += (mone1 * mone2);
+                        calcs.get(u).mehane1 += (mone1 * mone1);
+                        calcs.get(u).mehane2 += (mone2 * mone2);
+                    } else {
+                        calcs.put(u, new Calcs((mone1 * mone2), (mone1 * mone1), (mone2 * mone2)));
+                    }
+                }
+            }
+
+        }
+         it = calcs.entrySet().iterator();
         while (it.hasNext()) { //all movies curr rank
             Map.Entry pair = (Map.Entry) it.next(); // pair = key-movieID val-rankBycurr
             {
 
-            }
-            Double rA = (Double) pair.getValue(); //rank curr movie by curr user
-            mone1 = rA - currAVG;
-            //if(calcs.containsKey())
-
-        }
-        return 3;
     }
-
-
-
 }
