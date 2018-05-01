d196cdb52e175e9051d00ad35e16e2ee95da9f01
nogahm <nogahm@post.bgu.ac.il>
2018-05-01 11:40:07 +0300

    a

--- src/main.java
+++ src/main.java
@@ -1,2 +1,20 @@
-public class main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class main extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
+        primaryStage.setTitle("Everything4Rent");
+        primaryStage.setScene(new Scene(root, 550, 400));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
