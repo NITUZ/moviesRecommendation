6355e8d23870cf1f189450930a41956dd3b54a5f
karinMishor <karinmi@post.bgu.ac.il>
2018-05-02 09:35:27 +0300

    y

--- src/Recommend.java
+++ src/Recommend.java
@@ -5,34 +5,34 @@ public class Recommend {
     HashMap<Integer, Double> simUser2User; //key= userID , val= similarity
     HashMap<Integer, Double> MovieScore; //key-movieID, val-score
     HashMap<Integer, Calcs> calcs;
-    HashMap<Integer,Double> MovieToOffer;
+    HashMap<Integer, Double> MovieToOffer;
     User currentUser;
 
-    public static void main(){
-        Movie m1=new Movie(1,"1");
-        m1.avgRank=3;
-        Movie m2=new Movie(2,"2");
-        m2.avgRank=2;
-        Movie m3= new Movie(3,"3");
-        m3.avgRank=4;
-        User u1=new User(1,3);
-        User u2= new User(2,4);
-        User u3= new User(3,2);
+    public static void main(String[] args) {
+        Movie m1 = new Movie(1, "1");
+        m1.avgRank = 3;
+        Movie m2 = new Movie(2, "2");
+        m2.avgRank = 2;
+        Movie m3 = new Movie(3, "3");
+        m3.avgRank = 4;
+        User u1 = new User(1, 3);
+        User u2 = new User(2, 4);
+        User u3 = new User(3, 2);
         m1.addUser(u1);
         m1.addUser(u2);
         m1.addUser(u3);
         m2.addUser(u1);
         m2.addUser(u3);
         m3.addUser(u2);
-        u1.addMovie(m1.movieID,2);
-        u1.addMovie(m2.movieID,4);
-        u2.addMovie(m1.movieID,4);
-        u2.addMovie(m3.movieID,4);
-        u3.addMovie(m1.movieID,1);
-        u3.addMovie(m2.movieID,3);
-        Recommend r= new Recommend(u3);
-
-
+        u1.addMovie(m1.movieID, 2);
+        u1.addMovie(m2.movieID, 4);
+        u2.addMovie(m1.movieID, 4);
+        u2.addMovie(m3.movieID, 4);
+        u3.addMovie(m1.movieID, 1);
+        u3.addMovie(m2.movieID, 3);
+        Recommend r = new Recommend(u3);
+        r.calcSimUser2Users();
+        r.findBestRankMovies();
 
 
     }
@@ -45,6 +45,7 @@ public class Recommend {
         calcs = new HashMap<Integer, Calcs>();
         MovieToOffer = new HashMap<Integer, Double>();
     }
+
     //sort Similarity hashmap by value
     public void sortByValuesSim(HashMap map) {
         List list = new LinkedList(map.entrySet());
@@ -52,7 +53,7 @@ public class Recommend {
         Collections.sort(list, new Comparator() {
             public int compare(Object o1, Object o2) {
                 if (((double) ((Map.Entry) (o2)).getValue()) -
-                        (((double) ((Map.Entry) (o1)).getValue())) >0)
+                        (((double) ((Map.Entry) (o1)).getValue())) > 0)
                     return 1;
                 if (((double) ((Map.Entry) (o2)).getValue()) -
                         (((double) ((Map.Entry) (o1)).getValue())) < 0)
@@ -61,15 +62,16 @@ public class Recommend {
             }
         });
         HashMap sortedHashMap = new LinkedHashMap();
-        for (Iterator it = list.iterator(); it.hasNext();) {
+        for (Iterator it = list.iterator(); it.hasNext(); ) {
             Map.Entry entry = (Map.Entry) it.next();
             sortedHashMap.put(entry.getKey(), entry.getValue());
-        }int i=0;
-        simUser2User=new HashMap<Integer, Double>();
+        }
+        int i = 0;
+        simUser2User = new HashMap<Integer, Double>();
         Iterator it = sortedHashMap.entrySet().iterator();
         while (it.hasNext()/*&& i<50*/) {
             Map.Entry pair = (Map.Entry) it.next();
-            simUser2User.put((Integer)pair.getKey(), ((double) pair.getValue()));
+            simUser2User.put((Integer) pair.getKey(), ((double) pair.getValue()));
             i++;
         }
     }
@@ -115,7 +117,7 @@ public class Recommend {
         Collections.sort(list, new Comparator() {
             public int compare(Object o1, Object o2) {
                 if (((double) ((Map.Entry) (o2)).getValue()) -
-                        (((double) ((Map.Entry) (o1)).getValue())) >0)
+                        (((double) ((Map.Entry) (o1)).getValue())) > 0)
                     return 1;
                 if (((double) ((Map.Entry) (o2)).getValue()) -
                         (((double) ((Map.Entry) (o1)).getValue())) < 0)
@@ -124,39 +126,46 @@ public class Recommend {
             }
         });
         HashMap sortedHashMap = new LinkedHashMap();
-        for (Iterator it = list.iterator(); it.hasNext();) {
+        for (Iterator it = list.iterator(); it.hasNext(); ) {
             Map.Entry entry = (Map.Entry) it.next();
             sortedHashMap.put(entry.getKey(), entry.getValue());
-        }int i=0;
-        MovieToOffer=new HashMap<Integer, Double>();
+        }
+        int i = 0;
+        MovieToOffer = new HashMap<Integer, Double>();
         Iterator it = sortedHashMap.entrySet().iterator();
-        while (it.hasNext()&& i<10) {
+        while (it.hasNext() && i < 10) {
             Map.Entry pair = (Map.Entry) it.next();
-            MovieToOffer.put((Integer)pair.getKey(), ((double) pair.getValue()));
+            MovieToOffer.put((Integer) pair.getKey(), ((double) pair.getValue()));
             i++;
         }
     }
 
     //predict which score current users give to all the movies he didn`t watch
-    public void findBestRankMovies(){
+    public void findBestRankMovies() {
 
         Iterator it = ReadFromDB.movies.entrySet().iterator();
         while (it.hasNext()) { //all movies
             Map.Entry pair = (Map.Entry) it.next(); // pair = key-movieID val-Movie
-            Movie currMovie=((Movie)pair.getValue());
-            if(!currMovie.users.contains(currentUser)){
-                Double ScoreForItem=0.0, calculate1=0.0,calculate2=0.0;
-                for (User u:currMovie.users) {
+            Movie currMovie = ((Movie) pair.getValue());
+            if (!currMovie.users.contains(currentUser)) {
+                Double ScoreForItem = 0.0, calculate1 = 0.0, calculate2 = 0.0;
+                for (User u : currMovie.users) {
                     if (simUser2User.containsKey(u.userID)) { //if user u is a neighber of current user
-                         calculate1+=((u.MoviesRanks.get(currMovie.movieID) - u.avgRank) * simUser2User.get(u));
-                         calculate2+=simUser2User.get(u);
+                        calculate1 += ((u.MoviesRanks.get(currMovie.movieID) - u.avgRank) * simUser2User.get(u));
+                        calculate2 += simUser2User.get(u);
                     }
                 }
-                ScoreForItem=currentUser.avgRank+(calculate1)/calculate2;
-                MovieToOffer.put(currMovie.movieID,ScoreForItem);
+                ScoreForItem = currentUser.avgRank + (calculate1) / calculate2;
+                MovieToOffer.put(currMovie.movieID, ScoreForItem);
             }
 
-            }
-            sortByValuesScores(MovieToOffer);
+        }
+        sortByValuesScores(MovieToOffer);
+        Iterator it3 = MovieToOffer.entrySet().iterator();
+        while (it.hasNext()) { //all movies
+            Map.Entry pair = (Map.Entry) it.next(); // pair = key-movieID val-Movie
+            System.out.println(pair.getKey().toString() + "  " + pair.getValue().toString());
+
+        }
     }
 }
